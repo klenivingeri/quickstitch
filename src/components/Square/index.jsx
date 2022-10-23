6b024a6b2637f3964c14af248eb22e7f0d461142
Erick Kleniving <libertti@live.com>
2022-10-22 21:02:38 -0300

    chore: square ajustado

--- src/components/Square/index.jsx
+++ src/components/Square/index.jsx
@@ -1,7 +1,11 @@
 import { Container } from './style'
+import { isMobile } from 'react-device-detect'
 
 const Square = () => {
-  return (<Container>123</Container>)
+  return (
+  <Container isMobile={isMobile}>
+    <span>01</span>
+  </Container>)
 }
 
 export default Square
--- src/components/Square/style.js
+++ src/components/Square/style.js
@@ -1,7 +1,31 @@
 import styled from 'styled-components'
 
+
 export const Container = styled.div`
+font-family: 'Teko', sans-serif;
 height: 100%;
 width: 100%;
-background-color: blue;
+padding-top: ${({isMobile}) => isMobile ? 30 : 55 }px;
+font-size: ${({isMobile}) => isMobile ? 25 : 55 }rem;
+display: flex;
+font-weight: 500;
+line-height: 0px;
+justify-content: center;
+align-items: center;
+overflow: hidden;
+-webkit-touch-callout: none;  /* iPhone OS, Safari */
+-webkit-user-select: none;    /* Chrome, Safari 3 */
+-khtml-user-select: none;     /* Safari 2 */
+-moz-user-select: none;       /* Firefox */
+-ms-user-select: none;        /* IE10+ */
+user-select: none;            /* Possível implementação no futuro */
+&:first-of-type {
+  border-right: ${({isMobile}) => isMobile ? 1 : 4 }px solid white;
+}
+@media (max-width: 768px) {
+  &:first-of-type {
+    border-bottom: 1px solid white;
+  }
+}
 `
+
